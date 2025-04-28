d88317e59ce6420a7c59fafee0bcebf152cf3879
Zeal Patel <98937132+zealp25@users.noreply.github.com>
2025-04-28 13:25:44 -0500

    Create MyLibraryClass.cs

---
+++ MyLibraryClass.cs
@@ -0,0 +1,10 @@
+namespace SharedLibrary
+{
+    public class MyLibraryClass
+    {
+        public static string GetMessage()
+        {
+            return "Hello from SharedLibrary!";
+        }
+    }
+}
